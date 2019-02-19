902f0cd65064195332743964affa163a70ce4861
milosz <milosz@cherrypickgames.com>
2019-02-19 21:19:51 +0100

    after rebase fixes

---
+++ Assets/Scripts/MainController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainController : MonoBehaviour
+{
+    [SerializeField]
+    private TilesDatabase tilesDatabase;
+
+    [SerializeField]
+    private float speed =  0.125f;
+
+    private Coroutine rollCoroutine;
+
+    private int index;
+
+    private void Start()
+    {
+        index = 0;
+
+        if (tilesDatabase != null)
+            tilesDatabase.Fill();
+
+        if (rollCoroutine != null)
+        {
+            StopCoroutine(rollCoroutine);
+            rollCoroutine = null;
+        }
+
+        rollCoroutine = StartCoroutine(RollTheMusic());
+    }
+
+    private IEnumerator RollTheMusic()
+    {
+        yield return new WaitForSeconds(speed);
+
+        Interact(tilesDatabase.GetTilesColumns()[HandleIndex()]);
+        index++;
+
+        rollCoroutine = StartCoroutine(RollTheMusic());
+    }
+
+    private int HandleIndex()
+    {
+        if (index > 15)
+        {
+            index = 0;
+        }
+
+        return index;
+    }
+
+    private void Interact(TilesColumn tilesColumn)
+    {
+        foreach (AudibleTile tile in tilesColumn.tiles)
+        {
+            if (tile.IsActive)
+            {
+                tile.gameObject.GetComponent<Animator>().Play("Play");
+                tile.gameObject.GetComponent<AudioSource>().Play();
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/TilesDatabase.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TilesDatabase : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject audibleTile;
+
+    [SerializeField]
+    private InputController inputController;
+
+    [SerializeField]
+    private List<AudioClip> notes;
+
+    [SerializeField]
+    private List<TilesColumn> tilesColumns;
+
+
+    public List<TilesColumn> GetTilesColumns()
+    {
+        if (tilesColumns != null)
+            return tilesColumns;
+
+        return new List<TilesColumn>();
+    }
+
+    public void Fill()
+    {
+        TilesColumn column;
+        int k = 0;
+
+        for (int i = 0; i < 16; i++)
+        {
+            column = new TilesColumn();
+            column.tiles = new List<AudibleTile>();
+
+            for (int j = 0; j < 16; j++)
+            {
+                GameObject currentTile = Instantiate(audibleTile, new Vector3(i * 0.4f, j * 0.4f, 0), Quaternion.identity, gameObject.transform);
+                currentTile.GetComponent<AudioSource>().clip = notes[j];
+                column.tiles.Add(currentTile.GetComponent<AudibleTile>());
+                if (inputController != null)
+                    inputController.SubscribeForMouseUp(currentTile.GetComponent<AudibleTile>());
+
+                k++;
+            }
+            tilesColumns.Add(column);
+        }
+    }
+}
+
+[System.Serializable]
+public class TilesColumn
+{
+    public List<AudibleTile> tiles;
+}
